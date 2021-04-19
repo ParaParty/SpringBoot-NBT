0b02691a4faf93cff83a2584be5b1e2fc13a8b8a
Eric_Lian <git@superexercisebook.com>
2021-04-19 08:52:38 +0800

    feat: Spring Boot Integration

---
+++ src/main/java/com/superexercisebook/jackson/nbt/Nbt2HttpMessageConverter.java
@@ -0,0 +1,29 @@
+package com.superexercisebook.jackson.nbt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+
+public class Nbt2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+    /**
+     * Construct a new {@link Nbt2HttpMessageConverter} using default configuration
+     * provided by {@link Jackson2ObjectMapperBuilder}.
+     */
+    public Nbt2HttpMessageConverter() {
+        this(new NbtMapper());
+    }
+
+    /**
+     * Construct a new {@link Nbt2HttpMessageConverter} with a custom {@link ObjectMapper}.
+     * You can use {@link Jackson2ObjectMapperBuilder} to build it easily.
+     *
+     * @see Jackson2ObjectMapperBuilder#json()
+     */
+    public Nbt2HttpMessageConverter(ObjectMapper objectMapper) {
+        super(objectMapper, new MediaType("application", "nbt"));
+    }
+
+}
